cfc33479de93147f89ccb698856caef07fcb429e
jcisneros24 <jhonny.tauro24@gmail.com>
2016-12-26 01:00:39 -0500

    test
    test

--- AdminUserWS/AdminUserWS/src/pe/com/hiveit/eai/ws/factsys/adminuserws/service/AdminUserWSService.java
+++
@@ -1,8 +0,0 @@
-package pe.com.hiveit.eai.ws.factsys.adminuserws.service;
-
-import pe.com.hiveit.eai.ws.factsys.adminuserws.types.ConsultarUserRequest;
-import pe.com.hiveit.eai.ws.factsys.adminuserws.types.ConsultarUserResponse;
-
-public interface AdminUserWSService {
-	ConsultarUserResponse consultarUser(ConsultarUserRequest request);
-}
--- AdminUserWS/AdminUserWS/src/pe/com/hiveit/eai/ws/factsys/adminuserws/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://hiveit.com.pe/eai/ws/sisfact/loginws/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package pe.com.hiveit.eai.ws.factsys.adminuserws.types;
