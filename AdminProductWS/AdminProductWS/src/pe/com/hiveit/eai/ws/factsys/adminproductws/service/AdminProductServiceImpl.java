353e79311110db10bf634252da2a967ecf51137e
jcisneros24 <jhonny.tauro24@gmail.com>
2017-01-15 18:22:43 -0500

    150117

--- AdminProductWS/AdminProductWS/src/pe/com/hiveit/eai/ws/factsys/adminproductws/service/AdminProductServiceImpl.java
+++ AdminProductWS/AdminProductWS/src/pe/com/hiveit/eai/ws/factsys/adminproductws/service/AdminProductServiceImpl.java
@@ -12,25 +12,50 @@ public class AdminProductServiceImpl implements AdminProductService {
 	@Override
 	public ChangeCodeResponse changeCode(ChangeCodeRequest request) {
 		ChangeCodeResponse response = null;
-		UpdateCodeResponseBean validateUserBeanResponse = null;
+		UpdateCodeResponseBean updateCodeResponseBean = null;
 		
     	long tiempoInicio = System.currentTimeMillis();
     	String idTx ="" ;
     	String nomMetodo ="[changeCode - WS]";
     	String msjTx = "["+nomMetodo+ " idTx=" + idTx + "]";
-		
+    	
+    	//REQUEST
+    	String codArtiOld = request.getCodArtiOld();
+    	String codArtiNew = request.getCodArtiNew();
+    	
     	try {
 			response = new ChangeCodeResponse();
-			validateUserBeanResponse = new UpdateCodeResponseBean();
+			updateCodeResponseBean = new UpdateCodeResponseBean();
 			
-			UpdateCodeRequestBean validateUserBeanRequest = new UpdateCodeRequestBean();
-			validateUserBeanResponse = xrootdbDaoImpl.updateCode(validateUserBeanRequest);
+			if((codArtiOld !=null && !codArtiOld.isEmpty()) && (codArtiNew !=null && codArtiNew.isEmpty())){
+				UpdateCodeRequestBean updateCodeRequestBean = new UpdateCodeRequestBean();
+				updateCodeRequestBean.setCodArtiOld(codArtiOld);
+				updateCodeRequestBean.setCodArtiNew(codArtiNew);
+				
+				updateCodeResponseBean = xrootdbDaoImpl.updateCode(updateCodeRequestBean);
+				String codRptaUpdateCode = updateCodeResponseBean.getCodRpta();
+				String msgRptaUpdateCode = updateCodeResponseBean.getMsgRpta();
+				
+				if(codRptaUpdateCode.equals("0")){
+					System.out.println(codRptaUpdateCode);
+					System.out.println(msjTx + msgRptaUpdateCode);
+				}else{
+					System.out.println(msjTx + msgRptaUpdateCode);
+				}
+			}else{
+				System.out.println("Datos ingresados no validos");
+			}						
 			
 		} catch (DBException e) {
-			// TODO: handle exception
+			response.setCodigoRespuesta(e.getCode());
+			response.setCodigoRespuesta(e.getMessage());
 		} catch (Exception e) {
-			// TODO: handle exception
-		}
+			response.setCodigoRespuesta(e.getLocalizedMessage());
+			response.setCodigoRespuesta(e.getMessage());
+		}finally{
+			long tiempoFin = System.currentTimeMillis();			
+    		System.out.println(tiempoFin-tiempoInicio);
+    	}
 		return response;
 	}
 
